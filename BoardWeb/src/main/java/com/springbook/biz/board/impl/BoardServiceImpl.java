1d8f22a44dbe1be3b419bfc5f7a88f93cffb6cfc
hyhygg <yhyhy94@gmail.com>
2021-08-05 14:25:39 +0900

    프레젠테이션 레이어/비즈니스 레이어 통합 408

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -17,11 +17,7 @@ public class BoardServiceImpl implements BoardService {
 	private BoardDAO boardDAO;
 	
 	public void insertBoard(BoardVO vo) {
-//		if(vo.getSeq() == 0) {
-//			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
-//		}
-		boardDAO.insertBoard(vo);  // 100번 글 등록 성공
-//		boardDAO.insertBoard(vo);  // Exception 발생
+		boardDAO.insertBoard(vo);
 	}
 	
 	public void updateBoard(BoardVO vo) {
